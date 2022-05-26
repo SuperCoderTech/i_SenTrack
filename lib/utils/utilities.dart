114ed7ed0de6e29fc9e709496c0b9d721115ebe5
FlutLab.io <>
2022-05-26 18:11:56 +0000

    Autosave

--- lib/utils/utilities.dart
+++ lib/utils/utilities.dart
@@ -1,4 +1,7 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final Future<SharedPreferences> prefs = SharedPreferences.getInstance();
 
 Future<Position> determinePosition() async {
   bool serviceEnabled;
